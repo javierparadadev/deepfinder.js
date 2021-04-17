94f0e22523b1874ede65cc3dae8712bf8197d9e3
Javier Parada <javierparada73@outlook.com>
2021-04-17 12:05:03 +0200

    Add basic test (by types)

--- test/unit/deep-get-by-type.spec.js
+++ test/unit/deep-get-by-type.spec.js
@@ -4,15 +4,30 @@ const { deepGet } = require('../../index')
 
 describe("unit.deepGet.types", () => {
     it("Should find basic number", () => {
-        data = 39
+        const data = 39
         const result = deepGet(data, '')
         expect(result).to.be.equals(39)
     })
+
     it("Should find basic number inside object", () => {
-        data = {
+        const data = {
             value: 39,
         }
         const result = deepGet(data, 'value')
         expect(result).to.be.equals(39)
+    })
+
+    it("Should find basic string", () => {
+        const data = 'test str'
+        const result = deepGet(data, '')
+        expect(result).to.be.equals('test str')
+    })
+
+    it("Should find basic string inside object", () => {
+        const data = {
+            value: 'test str',
+        }
+        const result = deepGet(data, 'value')
+        expect(result).to.be.equals('test str')
     })
 })
