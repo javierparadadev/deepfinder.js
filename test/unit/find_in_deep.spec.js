4a1fae94fceba1c722922cfd9d39479af880e64d
Javier Parada <javierparada73@outlook.com>
2021-04-18 09:51:23 +0200

    Add deep find tests

---
+++ test/unit/find_in_deep.spec.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai')
+
+const { deepFind } = require('../../index')
+
+describe("unit.deepFind.types", () => {
+    it("Should return value with 0 depth lvls", () => {
+        const data = {
+            value: 39,
+        }
+        const result = deepFind(data, '')
+        expect(result).to.be.deep.equals(data)
+    })
+
+    it("Should return value with 1 depth lvl", () => {
+        const data = {
+            value: 39,
+        }
+        const result = deepFind(data, 'value')
+        expect(result).to.be.equals(data.value)
+    })
+
+    it("Should return value with 2 depth lvls", () => {
+        const data = {
+            subdata: {
+                value: 39,
+            },
+        }
+        const result = deepFind(data, 'subdata.value')
+        expect(result).to.be.equals(data.subdata.value)
+    })
+
+    it("Should return value with 3 depth lvls", () => {
+        const data = {
+            subdata: {
+                subsubdata: {
+                    value: 39,
+                },
+            },
+        }
+        const result = deepFind(data, 'subdata.subsubdata.value')
+        expect(result).to.be.equals(data.subdata.subsubdata.value)
+    })
+})
