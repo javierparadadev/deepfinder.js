5de1340965c75b4b79f213ef6bc0f4424f433b5b
Javier Parada <javierparada73@outlook.com>
2021-04-24 09:12:18 +0200

    More tests.

--- test/unit/find-in-deep-lists.spec.js
+++ test/unit/find-in-deep-lists.spec.js
@@ -60,4 +60,48 @@ describe("unit.deepFind.lists", () => {
         const result = deepFind(data, 'values.?.value')
         expect(result).to.be.deep.equals('b')
     })
+
+    it("Should return complete lists inside list", () => {
+        const data = {
+            allValues: [
+                { values: ['a']},
+                { values: ['b', 'c', 'd']},
+                { values: ['e']},
+            ],
+        }
+        const result = deepFind(data, 'allValues.*.values')
+        expect(result).to.be.deep.equals([['a'], ['b', 'c', 'd'], ['e']])
+    })
+
+    it("Should return complete lists inside direct list", () => {
+        const data = {
+            allValues: [['a'], ['b', 'c', 'd'], ['e']]
+        }
+        const result = deepFind(data, 'allValues.*.*')
+        expect(result).to.be.deep.equals([['a'], ['b', 'c', 'd'], ['e']])
+    })
+
+    it("Should return first value of all lists inside list", () => {
+        const data = {
+            allValues: [['a'], ['b', '3', '4'], ['c']]
+        }
+        const result = deepFind(data, 'allValues.*.0')
+        expect(result).to.be.deep.equals(['a', 'b', 'c'])
+    })
+
+    it("Should return existing path of lists inside direct list", () => {
+        const data = {
+            allValues: [['a'], ['b', 'c', 'd'], ['e']]
+        }
+        const result = deepFind(data, 'allValues.?.2')
+        expect(result).to.be.deep.equals('d')
+    })
+
+    it("Should return existing path inside existing path of lists inside list", () => {
+        const data = {
+            allValues: [['a'], ['b', { correct: 'correct' }, 'd'], ['e']]
+        }
+        const result = deepFind(data, 'allValues.?.?.correct')
+        expect(result).to.be.deep.equals('correct')
+    })
 })
